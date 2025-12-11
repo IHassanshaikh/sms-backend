66a7f6dc0280507c6dbbe3f2b81b2679bd3f3b81
Muhammad Hassan <hassanshaikh9717@gmail.com>
2025-12-12 01:18:12 +0500

    Fix Render deployment: Add start script and dynamic PORT

--- server.js
+++ server.js
@@ -22,4 +22,5 @@ app.get("/", (req, res) => {
     res.send("SMS System Backend is Running");
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
